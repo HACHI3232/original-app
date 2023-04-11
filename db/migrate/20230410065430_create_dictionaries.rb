30986f4c241cc0ccf9c4039e1c7cccd1a273d456
HACHI3232 <harada.chisato526@gmail.com>
2023-04-11 20:39:22 +0900

    add: migrate dictionaries

--- db/migrate/20230410065430_create_dictionaries.rb
+++ db/migrate/20230410065430_create_dictionaries.rb
@@ -1,7 +1,8 @@
 class CreateDictionaries < ActiveRecord::Migration[6.0]
   def change
     create_table :dictionaries do |t|
-
+      t.string :word,    null:false
+      t.text   :mean,    null:false
       t.timestamps
     end
   end
