5f1a94de98d563630f485c2896a87636ba0f7793
HACHI3232 <harada.chisato526@gmail.com>
2023-04-11 20:40:33 +0900

    add dictionaries setting

--- app/controllers/dictionaries_controller.rb
+++ app/controllers/dictionaries_controller.rb
@@ -1,4 +1,24 @@
 class DictionariesController < ApplicationController
   def index
+    @dictionaries = Dictionary.all
+  end
+
+  def new
+    @dictionaries = Dictionary.new
+  end
+
+  def create
+    Dictionary.create(dictionary_params)
+  end
+
+  def destroy
+    dictionary = Dictionary.find(params[:id])
+    dictionary.destroy
+  end
+
+private
+
+  def dictionary_params
+    params.require(:dictionary).permit(:word,:mean)
   end
 end
--- app/models/dictionary.rb
+++ app/models/dictionary.rb
@@ -1,2 +1,4 @@
 class Dictionary < ApplicationRecord
+  validates:word,presence: true
+  validates:mean,presence: true
 end
